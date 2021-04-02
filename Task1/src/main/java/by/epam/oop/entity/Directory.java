f515718c4962659d8dd9dfad30c8ba947b305b65
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-04-02 23:06:56 +0300

    Add files via upload

--- Task1/src/main/java/by/epam/oop/entity/Directory.java
+++ Task1/src/main/java/by/epam/oop/entity/Directory.java
@@ -1,5 +1,16 @@
 package by.epam.oop.entity;
 
 public class Directory {
-
+	private String path; 	// C:/dir/
+	private String name; // newdir
+	
+	public Directory(String path, String name) {
+		this.path = path;
+		this.name = name;
+	}
+	
+	public Directory() {
+		
+	}
+	
 }
