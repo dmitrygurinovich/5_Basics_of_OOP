4a64cfb2c232d47856aba3f04da9f8068747f801
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-04-03 18:22:04 +0300

    Task done

--- Task1/src/main/java/by/epam/oop/main/Main.java
+++ Task1/src/main/java/by/epam/oop/main/Main.java
@@ -25,5 +25,6 @@ public class Main {
 		
 		editor.deleteContent(textFile);
 		view.print(textFile);
+
 	}
 }
---
+++ Task2/src/main/java/by/epam/oop/main/App.java
@@ -0,0 +1,11 @@
+package by.epam.oop.main;
+
+/**
+ *  2. Создать класс Payment с внутренним классом, с помощью объектов которого можно сформировать покупку из
+ * 	нескольких товаров.
+ */
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
