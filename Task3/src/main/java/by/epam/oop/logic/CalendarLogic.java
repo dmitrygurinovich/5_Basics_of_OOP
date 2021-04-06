641ba606c9969b84893bada9c11dc887ad8f0ce8
Dmitry Gurinovich <dmitry.gurinovich@hotmail.com>
2021-04-06 21:51:31 +0300

    Task's done

--- Task3/src/main/java/by/epam/oop/logic/CalendarLogic.java
+++ Task3/src/main/java/by/epam/oop/logic/CalendarLogic.java
@@ -1,13 +1,15 @@
 package by.epam.oop.logic;
 
+import java.util.Calendar;
+
 public class CalendarLogic {
 
 	public CalendarLogic() {
+
+	}
+
+	public void addDate(Calendar calendar) { 
 		
 	}
-	
-	/*
-	 * public void addDate(Calendar calendar, Day date) { calendar. }
-	 */
-	
+
 }
